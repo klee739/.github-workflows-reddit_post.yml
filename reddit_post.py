fffe338d96c5984699c68ffb52c43a9fc6c9bebd
klee739 <70025605+klee739@users.noreply.github.com>
2025-08-27 11:32:22 -0400

    Update reddit_post.py

--- reddit_post.py
+++ reddit_post.py
@@ -11,14 +11,10 @@ reddit = praw.Reddit(
 )
 
 # Post details
-#title = "Use my link for free FSD!"
-#body = "Use my link to get 3 months of Full Self-Driving (Supervised) or $400 off Solar Panel installation! https://www.tesla.com/referral/kyeu702786"
-#subreddits = ["teslareferralcode", "TeslaReferralsCode"]
+title = "Use my link for $250 Tesla credit or free FSD!"
+body = "Use my link to get $250 in Tesla credit ot 3 months of Full Self-Driving (Supervised) or $400 off Solar Panel installation! https://www.tesla.com/referral/kyeu702786"
+subreddits = ["teslareferralcode", "TeslaReferralsCode"]
 
-#Amazon
-title = "Get $200 by signing up for Amazon VISA using my link!"
-body = "Use my link to get $200 when you get approved for Prime Visa! https://www.amazon.com/dp/BT00LN946S?externalReferenceId=b0424843-0799-4cd6-839d-5b9d8c6cceac"
-subreddits = ["ReferalLinks"]
 
 
 # Submit post to each subreddit
